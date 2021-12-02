ea444180e6d66fcd4c3a37552e32ccac9a59b1c1
Christopher Lin <clin@bwh.harvard.edu>
2021-12-02 10:18:35 -0500

    Refactor, part 2 challenge

--- 1/1.jl
+++ 1/1.jl
@@ -3,22 +3,29 @@ f = open("1/input.txt","r")
 depths = map((x)-> parse(Int32,x), readdlm(f,String))
 close(f)
 
-function increases(cur,prev)
-    if cur > prev
-        return true
-    else
-        return false
+function count_increases(list)
+    n_increases = 0
+    for (idx,val) in enumerate(list)
+        if(idx > 1)
+            n_increases += val > list[idx-1]
+        end
     end
+    return n_increases
+end
+
+function moving_sum(list, window)
+    summed_list = zeros(size(list))
+    for i in 1:(size(list)[1] - window+1)
+        s = sum(list[i:i+window-1])
+        summed_list[i] = s
+    end
+    return summed_list
 end
 
 function main()
-    n_increases = 0
-    for (idx,val) in enumerate(depths)
-        if(idx > 1)
-            n_increases += increases(val,depths[idx-1])
-        end
-    end
-    print(n_increases)
+    println(string("number of increases: ",count_increases(depths)))
+    denoised = moving_sum(depths, 3)
+    println(string("n denoised increases: ", count_increases(denoised)))
 end
 
 main()
