61bec869e1086c0a9f2739afe6f859060bb204eb
Christopher Lin <clin@bwh.harvard.edu>
2021-12-02 11:35:45 -0500

    Add 2b

---
+++ 2/2.jl
@@ -0,0 +1,33 @@
+using DelimitedFiles
+
+function process_instructions(ins)
+    instructions = Vector{Tuple{String, Int32}}(undef,size(ins)[1])
+    for i in 1:size(ins)[1]
+        instructions[i] = (ins[i,1],parse(Int32,ins[i,2]))
+    end
+    return instructions
+end
+
+function exec_instruction(ins, cur_pos)
+    ins_dict = Dict(
+        "forward" => ((x,k) -> (x[1] + k,x[2])),
+        "back" => ((x,k) -> (x[1] - k,x[2])),
+        "up" => ((x,k) -> (x[1],x[2] - k)),
+        "down" => ((x,k) -> (x[1],x[2] + k)),
+    )
+    return ins_dict[ins[1]](cur_pos,ins[2])
+end
+
+function main()
+    f = open("2/input.txt","r")
+    instructions_raw = readdlm(f,String)
+    close(f)
+    instructions = process_instructions(instructions_raw)
+    cur_pos = (0,0)
+    for i in instructions
+        cur_pos = exec_instruction(i,cur_pos)
+    end
+    println(cur_pos)
+    println(cur_pos[1]*cur_pos[2])
+end
+main()
